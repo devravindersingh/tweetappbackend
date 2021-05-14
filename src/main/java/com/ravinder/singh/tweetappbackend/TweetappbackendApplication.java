00fc4f67367f5aeb52b5925b8c9c461015adec6e
devravindersingh <ravindersngh402@outlook.com>
2021-05-14 22:34:06 +0530

    Initial Commit

---
+++ src/main/java/com/ravinder/singh/tweetappbackend/TweetappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.ravinder.singh.tweetappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetappbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravinder/singh/tweetappbackend/TweetappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravinder.singh.tweetappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
